2fcb29a9311c4a06cd73f54199091ebc0a78d843
Mesaque José da Silva <mesaquesilva147@gmail.com>
2024-10-01 21:39:09 -0300

    Atualizando as heranças

--- Herenças/TesteVeiculos.java
+++ Herenças/TesteVeiculos.java
@@ -1,21 +1,24 @@
 
 public class TesteVeiculos {
 	public static void main(String[] args) {
-		Moto moto = new Moto("Honda", "XRE", 2020, false);
-		
-		System.out.println("Detalhes da moto: ");
-		moto.exibirDetalhes();
-		
 
+		Moto moto = new Moto("Honda", "XRE", 2020, false);
 		Carro carro = new Carro("Toyota", "Corola", 2004, 4);
-		
+		Moto moto2 = new Moto("Honda", "NX500", 2024, true);
+		Carro carro2 = new Carro("Volkswagen", "gol", 2004, 4);
+
 		System.out.println("Detalhes do carro: ");
 		carro.exibirDetalhes();
+
+		System.out.println("\nDetalhes da moto: ");
+		moto.exibirDetalhes();
 		
-		
+		System.out.println("\nDetalhes do carro: ");
+		carro2.exibirDetalhes();
+
+		System.out.println("\nDetalhes da moto: ");
+		moto2.exibirDetalhes();
+
 	}
-		
 
 }
-
-
